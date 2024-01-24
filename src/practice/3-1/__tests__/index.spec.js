b8a3df82af05a57f0bfdacc2eb0734589c23f8e7
Roger <worth22focus3000@gmail.com>
2024-01-24 23:16:39 +0800

    feat: add practice 3-1

---
+++ src/practice/3-1/__tests__/index.spec.js
@@ -0,0 +1,10 @@
+import { sum } from '../index'
+
+// Test Suite
+describe('3-1 sum', () => {
+  // Test Case
+  it('1 + 2 should be 3', () => {
+    // Assertion
+    expect(sum(1, 2)).toBe(3)
+  })
+})
