0f2a1d6d2ed33cc01981afa6d1bb1e044f8c946d
Roger <worth22focus3000@gmail.com>
2024-01-25 13:57:12 +0800

    feat: modify 3-42 test file

--- src/practice/3-42/__tests__/index.spec.js
+++ src/practice/3-42/__tests__/index.spec.js
@@ -1,6 +1,6 @@
 import { Dog } from '../index'
 
-describe('3-42 Dog', () => {
+describe('3-42 Matcher 被呼叫的哈士奇', () => {
   it('狗餓了會汪汪叫', () => {
     // Arrange
     const dog = new Dog('哈士奇')
@@ -10,6 +10,15 @@ describe('3-42 Dog', () => {
     dog.hungry()
 
     // Assertion
-    expect(spyWoof).toHaveBeenCalledTimes(1)
+    expect(spyWoof).toHaveBeenCalled() // 斷言目標是否有被呼叫
+    expect(spyWoof).toHaveBeenCalledWith('汪汪汪') // 斷言目標被傳入什麼參數
+    expect(spyWoof).toHaveBeenCalledTimes(1) // 斷言目標被呼叫的次數
+
+    // Act
+    dog.hungry()
+    dog.hungry()
+
+    // Assertion
+    expect(spyWoof).toHaveBeenCalledTimes(3)
   })
 })
